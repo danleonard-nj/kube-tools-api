f825d34e143f4664a4a914b2ea0548f9501da85a
Dan Leonard <dcl525@gmail.com>
2023-05-09 22:49:06 +0000

    Merged PR 1288: DHT22 Temperature Sensor
    - Arduino code for temp sensors
    - Arduino code updates
    - Fix imports in provider
    - DHT22 temperature and humidity sensor and key auth on Nest senor route

--- services/kube-tools/routes/nest.py
+++ services/kube-tools/routes/nest.py
@@ -31,7 +31,7 @@ async def get_thermostat(container: ServiceProvider):
     return await service.get_thermostat()
 
 
-@nest_bp.configure('/api/nest/sensor', methods=['POST'], auth_scheme=AuthPolicy.Default)
+@nest_bp.with_key_auth('/api/nest/sensor', methods=['POST'], key_name='nest-sensor-api-key')
 async def post_sensor_data(container: ServiceProvider):
     service: NestService = container.resolve(NestService)
 
--- services/kube-tools/scripts/esp8266_client/esp8266_client.ino
+++ services/kube-tools/scripts/esp8266_client/esp8266_client.ino
@@ -3,26 +3,50 @@
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecureBearSSL.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
-char* ssid     = "HotGiblets24";
-char* password = "vN55]8T7D7`+/W42I=BP:56CMXJ6$]w2";
-char* deviceId = "41651552-6c5c-4465-b5ed-4cd32d17772e";
-char* apiKey = "50384447-cc4c-485b-84fb-7057591fcea2";
-char* device_key = "bedroom-temp-monitor";
-char* base_url = "https://api.dan-leonard.com";
+#define DHTPIN 5      // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22 // DHT 22 (AM2302)
+
+char *ssid = "HotGiblets24";
+char *password = "vN55]8T7D7`+/W42I=BP:56CMXJ6$]w2";
+char *deviceId = "41651552-6c5c-4465-b5ed-4cd32d17772e";
+char *apiKey = "50384447-cc4c-485b-84fb-7057591fcea2";
+char *device_key = "bedroom-temp-monitor";
+char *base_url = "https://api.dan-leonard.com";
 
 int interval = 1000;
-const char* host = "djxmmx.net";
+const char *host = "djxmmx.net";
 const uint16_t port = 17;
 
+DHT dht(DHTPIN, DHTTYPE);
 ESP8266WiFiMulti wifiMulti;
 WiFiClient wifiClient;
 HTTPClient httpClient;
-  
-String build_json_payload(int degreesCelsius, char* deviceId) {
+
+struct SensorData
+{
+  float temperature;
+  float humidity;
+};
+
+SensorData get_sensor_data()
+{
+  SensorData data;
+
+  data.temperature = dht.readTemperature();
+  data.humidity = dht.readHumidity();
+
+  return data;
+}
+
+String build_json_payload(float degreesCelsius, float humidityPercent, char *deviceId)
+{
   JSONVar payload;
 
   payload["degrees_celsius"] = degreesCelsius;
+  payload["humidity_percent"] = humidityPercent;
   payload["sensor_id"] = deviceId;
 
   String jsonString = JSON.stringify(payload);
@@ -31,34 +55,38 @@ String build_json_payload(int degreesCelsius, char* deviceId) {
   return jsonString;
 }
 
-void connect_wifi() {
+void connect_wifi()
+{
   Serial.print("Connecting to: ");
   Serial.println(ssid);
-
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.print(".");
   }
 }
 
-void post_sensor_data(int degreesCelsius, char* deviceId) {
+void post_sensor_data(int degreesCelsius, char *deviceId)
+{
   HTTPClient https;
   WiFiClientSecure client;
 
   client.setInsecure();
-  
+
   Serial.println("Posting sensor data");
-  
+
   String url = String(base_url) + String("/api/tools/nest/sensor");
   Serial.println("Endpoint: ");
   Serial.print(url);
   Serial.println("");
 
-  String json = build_json_payload(degreesCelsius, deviceId);
+  SensorData data = get_sensor_data();
+
+  String json = build_json_payload(data.temperature, data.humidity, deviceId);
 
   https.begin(client, url);
   https.addHeader("Content-Type", "application/json");
@@ -69,15 +97,17 @@ void post_sensor_data(int degreesCelsius, char* deviceId) {
   String response = https.getString();
   Serial.println("Response:");
   Serial.println(response);
-  
+
   https.end();
 
   Serial.println("Status code: ");
   Serial.print(statusCode);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
+  dht.begin();
 
   connect_wifi();
 
@@ -85,7 +115,8 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
-void loop() {
+void loop()
+{
   post_sensor_data(0, deviceId);
   delay(interval);
 }
