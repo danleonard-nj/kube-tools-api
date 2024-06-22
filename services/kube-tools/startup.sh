8aade350c0fd654f7eb4de2c2471275c55e76e0c
Dan Leonard <dcl525@gmail.com>
2024-06-22 14:44:01 +0000

    Merged PR 1900: Worker Count
    Updated startup.sh

--- services/kube-tools/startup.sh
+++ services/kube-tools/startup.sh
@@ -1,2 +1,2 @@
 echo "Hello!  Starting up the server..."
-uvicorn --log-level=error --host 0.0.0.0 --port=80 --workers 1 app:app
+uvicorn --log-level=error --host 0.0.0.0 --port=80 --workers 2 app:app
